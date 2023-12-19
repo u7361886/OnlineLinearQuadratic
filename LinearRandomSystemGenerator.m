dc6b74ed160d6d2cda2cea3022bf4e8adabb8d3b
u7361886 <u7361886@anu.edu.au>
2023-12-20 01:11:35 +1100

    changes

--- LinearRandomSystemGenerator.m
+++ LinearRandomSystemGenerator.m
@@ -3,6 +3,7 @@ function [A,B,K0] = LinearRandomSystemGenerator(n,m,poleScale)
     B = zeros(n,m);
     count = 1;
     epsilon = 10^(-1);
+<<<<<<< Updated upstream
     while(rank(ctrb(A,B)) < n || count == 1 || min(svd(ctrb(A,B))) < epsilon)
         try
             count = 0;
@@ -14,6 +15,18 @@ function [A,B,K0] = LinearRandomSystemGenerator(n,m,poleScale)
                 K0 = -place(A,B,generateComplexNumbers(2*floor(n/2)).*poleScale);
             end
 %             disp(exception.message);
+=======
+    while(rank(ctrb(A,B)) < n || count == 1 || min(svd(ctrb(A,B))) > epsilon)
+        try
+            count = 0;
+            A = 10*rand(n,n);
+            B = 10*rand(n,m);
+            if(mod(n, 2) ~= 0)
+                K0 = -place(A,B,[generateComplexNumbers(floor(n/2)),rand(1)].*poleScale);
+            else
+                K0 = -place(A,B,generateComplexNumbers(floor(n/2)).*poleScale);
+            end
+>>>>>>> Stashed changes
         catch
             count = 1;
         end
